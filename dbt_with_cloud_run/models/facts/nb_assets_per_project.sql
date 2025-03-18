1013662fcf9aefaf80713bbce8750fedbc5cf22a
Amadou DEME <demepoulo@gmail.com>
2025-03-18 18:18:48 +0100

    ci/cd added

--- dbt_with_cloud_run/models/facts/nb_assets_per_project.sql
+++ dbt_with_cloud_run/models/facts/nb_assets_per_project.sql
@@ -4,10 +4,10 @@
     schema='kpis_dataset',
     )
 }}
--- Which clients have the highest total number of assets across all projects and what is the 95th percentile of their brand codes?
+-- Which clients have the highest total number of assets across all projects and what is the 95th percentile of their brand codes?d
 SELECT
   project_id,
-  count(asset_id) as total_assets,
+  count(asset_id) as total_assets
  
 FROM {{ ref('combined_brief_and_asset') }}
   
